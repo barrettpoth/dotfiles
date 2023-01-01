8be9be43ffcf054eeaac1dadc16a2a7ad58a5ced
Barrett Poth <barrett@rnwbl.com>
2023-01-01 10:42:25 -0600

    fix: fix colorscheme bug and remove unneeded configs

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,17 +1,14 @@
 require("bufferline").setup {
     options = {
         numbers = "none", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
-        indicator_icon = nil,
         indicator = { style = "icon", icon = "▎" },
-        buffer_close_icon = "",
         modified_icon = "●",
-        close_icon = "",
         left_trunc_marker = "",
         right_trunc_marker = "",
         max_name_length = 30,
         max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
         tab_size = 21,
-        diagnostics = "nvim_lsp", -- | "nvim_lsp" | "coc",
+        diagnostics = "nvim_lsp",
         diagnostics_update_in_insert = false,
         diagnostics_indicator = function(count, level)
             local icons = {
@@ -22,40 +19,32 @@ require("bufferline").setup {
             }
             return icons[level] .. ' ' .. count
         end,
-        -- NOTE: this will be called a lot so don't do any heavy processing here
-        -- custom_filter = function(buf_number)
-        --   -- filter out filetypes you don't want to see
-        --   if vim.bo[buf_number].filetype ~= "<i-dont-want-to-see-this>" then
-        --     return true
-        --   end
-        --   -- filter out by buffer name
-        --   if vim.fn.bufname(buf_number) ~= "<buffer-name-I-dont-want>" then
-        --     return true
-        --   end
-        --   -- filter out based on arbitrary rules
-        --   -- e.g. filter out vim wiki buffer from tabline in your work repo
-        --   if vim.fn.getcwd() == "<work-repo>" and vim.bo[buf_number].filetype ~= "wiki" then
-        --     return true
-        --   end
-        -- end,
         offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
         show_buffer_icons = true,
-        show_buffer_close_icons = true,
-        show_close_icon = true,
-        show_tab_indicators = true,
-        persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
-        -- can also be a table containing 2 custom separators
-        -- [focused and unfocused]. eg: { '|', '|' }
-        separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        show_tab_indicators = false,
+        persist_buffer_sort = true,
+        separator_style = 'thin',
         enforce_regular_tabs = true,
         always_show_bufferline = true,
-        -- sort_by = 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
-        --   -- add custom logic
-        --   return buffer_a.modified > buffer_b.modified
-        -- end
     },
 }
 
 -- Navigate buffers
 vim.keymap.set("n", "<S-l>", ":bnext<CR>")
 vim.keymap.set("n", "<S-h>", ":bprevious<CR>")
+vim.keymap.set("n", "gb", ":BufferLinePick<CR>")
+vim.keymap.set("n", "gB", ":BufferLinePickClose<CR>")
+vim.keymap.set("n", "<leader>1", ":BufferLineGoToBuffer 1<CR>")
+vim.keymap.set("n", "<leader>2", ":BufferLineGoToBuffer 2<CR>")
+vim.keymap.set("n", "<leader>3", ":BufferLineGoToBuffer 3<CR>")
+vim.keymap.set("n", "<leader>4", ":BufferLineGoToBuffer 4<CR>")
+vim.keymap.set("n", "<leader>5", ":BufferLineGoToBuffer 5<CR>")
+vim.keymap.set("n", "<leader>6", ":BufferLineGoToBuffer 6<CR>")
+vim.keymap.set("n", "<leader>7", ":BufferLineGoToBuffer 7<CR>")
+vim.keymap.set("n", "<leader>8", ":BufferLineGoToBuffer 8<CR>")
+vim.keymap.set("n", "<leader>9", ":BufferLineGoToBuffer 9<CR>")
+
+-- I'm sure there is a better way to do this, but bufferline colors are jacked setting colorscheme here
+vim.cmd.colorscheme('rose-pine')
