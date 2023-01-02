c46e560e605bc6a435ef61e2a82d8e03e776e0eb
Barrett Poth <barrett@rnwbl.com>
2023-01-02 09:45:50 -0600

    fix: map Q to macro
    The smartq plugin maps Q to recording a macro, since it maps q to closing
    buffer. This commit removes a left over mapping of Q to null that was
    conflicting with the plugin's Q mapping.

--- lua/barrettpoth/remap.lua
+++ lua/barrettpoth/remap.lua
@@ -16,9 +16,6 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
--- Q is a scary place
-vim.keymap.set("n", "Q", "<nop>")
-
 -- format buffer
 vim.keymap.set("n", "<localleader>f", vim.lsp.buf.format)
 
