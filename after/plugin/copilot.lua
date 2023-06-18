df3abc7e743ffe497a4bcd45fae603cbd4f006d1
Barrett Poth <barrett@rnwbl.com>
2023-06-17 23:40:39 -0500

    fix: copilot interference with dap-ui buffers

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -2,3 +2,18 @@ vim.keymap.set("i", "<A-]>", "<Plug>(copilot-next)")
 vim.keymap.set("i", "<A-[>", "<Plug>(copilot-previous)")
 vim.keymap.set("i", "<A-p>", "<Plug>(copilot-suggest)")
 vim.keymap.set("i", "<C-]>", "<Plug>(copilot-dismiss)")
+
+vim.g.copilot_filetypes = {
+    ['*'] = false,
+    ["python"] = true,
+    ["lua"] = true,
+    ["rust"] = true,
+    ["clojure"] = true,
+    ["R"] = true,
+    ["nu"] = true,
+    ["javascript"] = true,
+    ["typescript"] = true,
+    ["go"] = true,
+    ["ruby"] = true,
+    ["markdown"] = true,
+}
