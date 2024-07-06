7db9113c11d25c59c689aca4630dba125151c529
Barrett Poth <barrett@rnwbl.com>
2024-07-06 14:17:38 -0500

    mod

---
+++ nvim/.config/nvim/lua/lazy-plugins.lua
@@ -0,0 +1,89 @@
+-- [[ Configure and install plugins ]]
+--
+--  To check the current status of your plugins, run
+--    :Lazy
+--
+--  You can press `?` in this menu for help. Use `:q` to close the window
+--
+--  To update plugins you can run
+--    :Lazy update
+--
+require('lazy').setup({
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+
+  { 'numToStr/Comment.nvim', opts = {} },
+
+  require 'barrettpoth/plugins/gitsigns',
+
+  require 'barrettpoth/plugins/telescope',
+
+  require 'barrettpoth/plugins/lspconfig',
+
+  require 'barrettpoth/plugins/conform',
+
+  require 'barrettpoth/plugins/cmp',
+
+  require 'barrettpoth/plugins/theme',
+
+  require 'barrettpoth/plugins/todo-comments',
+
+  require 'barrettpoth/plugins/mini',
+
+  require 'barrettpoth/plugins/treesitter',
+
+  require 'barrettpoth/plugins/debug',
+
+  require 'barrettpoth/plugins/indent_line',
+
+  require 'barrettpoth/plugins/lint',
+
+  require 'barrettpoth/plugins/autopairs',
+
+  -- NOTE: everything above came recommended by kickstart-nvim, below are my additions
+
+  require 'barrettpoth/plugins/harpoon',
+
+  require 'barrettpoth/plugins/copilot',
+
+  require 'barrettpoth/plugins/git',
+
+  require 'barrettpoth/plugins/surround',
+
+  'farmergreg/vim-lastplace',
+
+  'camgraff/telescope-tmux.nvim',
+
+  require 'barrettpoth/plugins/markdown-preview',
+
+  'tpope/vim-rails',
+
+  require 'barrettpoth/plugins/autotag',
+
+  require 'barrettpoth/plugins/test',
+
+  require 'barrettpoth/plugins/tmux-nav',
+
+  require 'barrettpoth/plugins/pandoc',
+}, {
+  ui = {
+    -- If you are using a Nerd Font: set icons to an empty table which will use the
+    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+})
+
+-- vim: ts=2 sts=2 sw=2 et
