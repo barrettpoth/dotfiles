7f6bd8dd2bff84998f10cccc696e3638fefb5519
Barrett Poth <barrett@rnwbl.com>
2024-07-07 11:20:49 -0500

    feat: oil

--- nvim/.config/nvim/lua/lazy-plugins.lua
+++ nvim/.config/nvim/lua/lazy-plugins.lua
@@ -54,6 +54,8 @@ require('lazy').setup({
   require 'barrettpoth/plugins/test',
 
   require 'barrettpoth/plugins/tmux-nav',
+
+  require 'barrettpoth/plugins/oil',
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
