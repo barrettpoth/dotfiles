e1f030a2b04bde99413a9f486d6bfe583da946a9
Barrett Poth <barrett@rnwbl.com>
2024-07-21 12:22:42 -0500

    chore: spell

--- nvim/.config/nvim/lua/barrettpoth/plugins/theme.lua
+++ nvim/.config/nvim/lua/barrettpoth/plugins/theme.lua
@@ -1,29 +1,24 @@
--- return {
---   'projekt0n/github-nvim-theme',
---   priority = 1000,
---   config = function()
---     require('github-theme').setup {
---       options = {
---         styles = {
---           comments = 'italic',
---         },
---       },
---     }
---     vim.cmd [[colorscheme github_dark]]
---   end,
--- }
-
 return {
-  'folke/tokyonight.nvim',
+  'scottmckendry/cyberdream.nvim',
   lazy = false,
   priority = 1000,
-  opts = {},
   config = function()
-    vim.g.tokyonight_style = 'night'
-    vim.g.tokyonight_italic_functions = true
-    vim.g.tokyonight_italic_variables = true
-    vim.g.tokyonight_italic_keywords = true
-    vim.g.tokyonight_italic_comments = true
-    vim.cmd [[colorscheme tokyonight]]
+    require('cyberdream').setup {
+      transparent = true,
+      italic_comments = true,
+      hide_fillchars = false,
+      borderless_telescope = false,
+      terminal_colors = true,
+      theme = {
+        variant = 'dark',
+      },
+      extensions = {
+        telescope = true,
+        notify = true,
+        mini = true,
+      },
+    }
+
+    vim.cmd [[colorscheme cyberdream]]
   end,
 }
