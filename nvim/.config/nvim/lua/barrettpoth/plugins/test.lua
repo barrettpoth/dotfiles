d9bc3562577015325c36d4496f08e88adbc33ab2
Barrett Poth <barrett@rnwbl.com>
2025-06-11 15:49:53 -0500

    fix: neotest keybindings

--- nvim/.config/nvim/lua/barrettpoth/plugins/test.lua
+++ nvim/.config/nvim/lua/barrettpoth/plugins/test.lua
@@ -10,6 +10,14 @@ return {
       'olimorris/neotest-rspec',
       'nvim-neotest/nvim-nio',
     },
+    opts = {
+      status = {
+        virtual_text = true,
+        signs = true,
+        update_in_insert = false,
+        underline = true,
+      },
+    },
     config = function()
       local neotest = require 'neotest'
 
@@ -28,8 +36,10 @@ return {
         vim.keymap.set('n', keys, func, { desc = desc })
       end
 
-      nmap('<C-t>', neotest.run.run, 'Run the nearest test')
-      nmap('<S-t>', neotest.summary.toggle, 'Toggle the summary window')
+      nmap('<C-t><C-t>', neotest.run.run, 'Run the nearest test')
+      nmap('<C-t><C-l>', neotest.run.run_last, 'Run the last test')
+      nmap('<C-t><C-s>', neotest.summary.toggle, 'Toggle the summary window')
+      nmap('<C-t><C-o>', neotest.output.open, 'Open the output window')
       nmap('[t', function()
         neotest.jump.prev { status = 'failed' }
       end, 'Jump to the next failed test')
