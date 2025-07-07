770c9bdf6d0981ba13d4cc6f7dcddb6d9bc62c4b
Barrett Poth <barrett@rnwbl.com>
2025-07-07 16:29:49 -0500

    nvim: remove modeline lines

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -3,6 +3,3 @@ vim.opt.textwidth = 80
 
 -- set spell
 vim.opt.spell = true
-
--- -- set <localleader>f to format the current buffer using gq
--- vim.api.nvim_set_keymap('n', '<localleader>f', ':normal gg<CR> gqG<CR>', { noremap = true, silent = true })
