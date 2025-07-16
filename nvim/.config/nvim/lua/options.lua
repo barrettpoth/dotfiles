915f6f6547ad947edbb789fd2a9de3b23b9313d9
Barrett Poth <barrett@rnwbl.com>
2025-07-16 16:25:31 -0500

    Transparent background

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -56,6 +56,9 @@ vim.opt.inccommand = 'split'
 -- Show which line your cursor is on
 vim.opt.cursorline = true
 
+-- Set line length visual indicator
+vim.opt.colorcolumn = "120"
+
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
