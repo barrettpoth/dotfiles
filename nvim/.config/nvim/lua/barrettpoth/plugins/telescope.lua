f142430d6f6504108d31a4ee082782ff30fa02ee
Barrett Poth <barrett@rnwbl.com>
2025-07-16 16:24:52 -0500

    Add <leader>sa to telescope search all files (include dotfiles)

--- nvim/.config/nvim/lua/barrettpoth/plugins/telescope.lua
+++ nvim/.config/nvim/lua/barrettpoth/plugins/telescope.lua
@@ -69,6 +69,7 @@ return {
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Search [F]iles' })
+      vim.keymap.set('n', '<leader>sa', ':lua require"telescope.builtin".find_files({ hidden = true })<CR>', { desc = 'Search [F]iles' })
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
