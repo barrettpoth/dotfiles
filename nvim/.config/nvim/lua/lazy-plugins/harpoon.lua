fbef78bacdf6bf4b2271c3be8431bf641f106d0b
Barrett Poth <barrett@rnwbl.com>
2025-07-28 16:45:37 -0500

    Put harpoon keymaps under `<localleader>`

--- nvim/.config/nvim/lua/lazy-plugins/harpoon.lua
+++ nvim/.config/nvim/lua/lazy-plugins/harpoon.lua
@@ -7,56 +7,56 @@ return {
     harpoon:setup {}
 
     -- basic telescope configuration
-    vim.keymap.set('n', '<C-e>', function()
+    vim.keymap.set('n', '<localleader>e', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end)
 
-    vim.keymap.set('n', '<leader>a', function()
+    vim.keymap.set('n', '<localleader>a', function()
       harpoon:list():add()
     end)
 
-    vim.keymap.set('n', '<leader>1', function()
+    vim.keymap.set('n', '<localleader>1', function()
       harpoon:list():select(1)
     end)
 
-    vim.keymap.set('n', '<leader>2', function()
+    vim.keymap.set('n', '<localleader>2', function()
       harpoon:list():select(2)
     end)
 
-    vim.keymap.set('n', '<leader>3', function()
+    vim.keymap.set('n', '<localleader>3', function()
       harpoon:list():select(3)
     end)
 
-    vim.keymap.set('n', '<leader>4', function()
+    vim.keymap.set('n', '<localleader>4', function()
       harpoon:list():select(4)
     end)
 
-    vim.keymap.set('n', '<leader>5', function()
+    vim.keymap.set('n', '<localleader>5', function()
       harpoon:list():select(5)
     end)
 
-    vim.keymap.set('n', '<leader>6', function()
+    vim.keymap.set('n', '<localleader>6', function()
       harpoon:list():select(6)
     end)
 
-    vim.keymap.set('n', '<leader>7', function()
+    vim.keymap.set('n', '<localleader>7', function()
       harpoon:list():select(7)
     end)
 
-    vim.keymap.set('n', '<leader>8', function()
+    vim.keymap.set('n', '<localleader>8', function()
       harpoon:list():select(8)
     end)
 
-    vim.keymap.set('n', '<leader>9', function()
+    vim.keymap.set('n', '<localleader>9', function()
       harpoon:list():select(9)
     end)
 
     -- Toggle previous & next buffers stored within Harpoon list
-    vim.keymap.set('n', '<C-S-P>', function()
+    vim.keymap.set('n', '[e', function()
       harpoon:list():prev()
     end)
 
-    vim.keymap.set('n', '<C-S-N>', function()
+    vim.keymap.set('n', 'e]', function()
       harpoon:list():next()
     end)
   end,
