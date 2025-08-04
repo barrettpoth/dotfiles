d6ff35b05e1bf6ef10fcbd3e2db629f69dc653b0
Barrett Poth <barrett@rnwbl.com>
2025-08-03 22:02:24 -0500

    add % to mini.statusline

--- nvim/.config/nvim/lua/lazy-plugins/mini.lua
+++ nvim/.config/nvim/lua/lazy-plugins/mini.lua
@@ -3,12 +3,20 @@ return {
     'echasnovski/mini.nvim',
     config = function()
       local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
+
       statusline.setup { use_icons = vim.g.have_nerd_font }
 
       statusline.section_location = function()
-        return '%2l:%-2v'
+        local current_line = vim.api.nvim_win_get_cursor(0)[1]
+        local total_lines = vim.api.nvim_buf_line_count(0)
+        local percentage = math.floor((current_line / total_lines) * 100)
+
+        formmatted_location = '%l:%v'
+        formmatted_percent = string.format('%d', percentage) .. '%%'
+        return formmatted_location .. " | " .. formmatted_percent
       end
+
+      statusline.section_searchcount = function() end
 
       vim.api.nvim_set_hl(0, 'MiniStatuslineDevinfo', { fg = '#ff6e5e', bg = '#3c4048' })
       vim.api.nvim_set_hl(0, 'MiniStatuslineFilename', { fg = '#ffffff', bg = '#3c4048' })
